b870717a8cb8fe9d5f86195393c3544bf0b54f2b
guokui <guokui@comein.cn>
2022-02-10 18:05:52 +0800

    u

--- my-gateway/my-gateway-web/src/main/java/com/me/gateway/config/WebSecurityConfig.java
+++ my-gateway/my-gateway-web/src/main/java/com/me/gateway/config/WebSecurityConfig.java
@@ -1,9 +1,13 @@
 package com.me.gateway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /**
  * @author 郭奎
@@ -12,13 +16,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * 1、作用：放开请求  ，网关只做资源服务器进行权限校验
  **/
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebFluxSecurity
+public class WebSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .and().csrf().disable();
+    WebSecurityConfigurerAdapter
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        SecurityWebFilterChain chain = http.authorizeExchange()
+                // 白名单放行
+                .pathMatchers("/**").permitAll()
+                .and().csrf().disable().build();
+        // 设置自定义登录参数转换器
+
+        return chain;
+
     }
 }
